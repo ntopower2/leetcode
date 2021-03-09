c62719263cb7a641faa6d79fab3becaf349369d4
ntopower2 <n.to.the.power.of.2@gmail.com>
2021-03-09 21:16:03 +0200

    Problem 12 (no hashtables)
    Signed-off-by: ntopower2 <n.to.the.power.of.2@gmail.com>

--- editor/en/12IntegerToRoman.cpp
+++ editor/en/12IntegerToRoman.cpp
@@ -8,66 +8,37 @@ class Solution {
 public:
     string intToRoman(int num) {
         string res;
-        vector<pair<int, char>> nums = {
-                {1000, 'M'},
-                {500,  'D'},
-                {100,  'C'},
-                {50,   'L'},
-                {10,   'X'},
-                {5,    'V'},
-                {1,    'I'}
-        };
-        map<int, string> hundreds = {
-                {0, ""},
-                {1,"C"},
-                {2,"CC"},
-                {3,"CCC"},
-                {4,"CD"},
-                {5,"D"},
-                {6,"DC"},
-                {7,"DCC"},
-                {8,"DCCC"},
-                {9,"CM"}
-        };
-        map<int, string> tens = {
-                {0, ""},
-                {1,"X"},
-                {2,"XX"},
-                {3,"XXX"},
-                {4,"XL"},
-                {5,"L"},
-                {6,"LX"},
-                {7,"LXX"},
-                {8,"LXXX"},
-                {9,"XC"}
-        };
-        map<int, string> ones = {
-                {0, ""},
-                {1,"I"},
-                {2,"II"},
-                {3,"III"},
-                {4,"IV"},
-                {5,"V"},
-                {6,"VI"},
-                {7,"VII"},
-                {8,"VIII"},
-                {9,"IX"}
-        };
         string num_str = to_string(num);
         for (int i=0;i<num_str.size();i++) {
+            int d = num_str[i]-'0';
             switch (num_str.size()-i) {
                 case 4:
                     for (int j = 0; j < num_str[i]-'0'; j++)
                         res += 'M';
                     break;
                 case 3:
-                    res += hundreds[num_str[i]-'0'];
+                    if (d==4) res+="CD";
+                    else if (d==9) res+="CM";
+                    else {
+                        if (d>4) res+='D',d-=5;
+                        for (int j = 0; j < d; j++) res += 'C';
+                    }
                     break;
                 case 2:
-                    res += tens[num_str[i]-'0'];
+                    if (d==4) res+="XL";
+                    else if (d==9) res+="XC";
+                    else {
+                        if (d>4) res+='L',d-=5;
+                        for (int j = 0; j < d; j++) res += 'X';
+                    }
                     break;
                 case 1:
-                    res += ones[num_str[i]-'0'];
+                    if (d==4) res+="IV";
+                    else if (d==9) res+="IX";
+                    else {
+                        if (d>4) res+='V',d-=5;
+                        for (int j = 0; j < d; j++) res += 'I';
+                    }
                     break;
             }
         }
