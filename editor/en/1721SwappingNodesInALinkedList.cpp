9912b7830cbb234f94291e5b624d4013e12be658
ntopower2 <n.to.the.power.of.2@gmail.com>
2021-03-15 20:46:26 +0200

    Problem 1721 (only 4 pointers)
    Signed-off-by: ntopower2 <n.to.the.power.of.2@gmail.com>

--- editor/en/1721SwappingNodesInALinkedList.cpp
+++ editor/en/1721SwappingNodesInALinkedList.cpp
@@ -16,32 +16,58 @@ public:
     ListNode* swapNodes(ListNode* head, int k) {
         if (!head->next) return head;
         ListNode* curr = head, *first, *second, *prevf, *prevs;
-        ListNode* lst[(int)1e5];
-        int n = 0;
+        int n = 0, i=1;
+        if (k==1) prevf = nullptr, first = head;
         while (curr) {
-            lst[n++] = curr;
+            if (++i==k && k!=1)
+                prevf = curr, first = curr->next;
+            n++;
             curr = curr->next;
         }
         if (n==2) {
-            lst[0]->next= nullptr; lst[1]->next=lst[0];
-            return lst[1];
+            curr = head->next;
+            curr->next = head;
+            head->next = nullptr;
+            return curr;
         }
-        first = lst[(k>n/2)?n-k:k-1]; second = lst[(k>n/2)?k-1:n-k];
-        if (first!=lst[0])
-            prevf = lst[(k>n/2)?n-k-1:k-2], prevf->next = second;
-        if (second!=lst[0])
-            prevs=lst[(k>n/2)?k-2:n-k-1], prevs->next = first;
-        swap(first->next, second->next);
-        return (first==lst[0])?second:lst[0];
+        if (k>=n/2) {
+            i=1;
+            prevs = prevf;
+            second = first;
+            prevf = head; first=prevf->next;
+            if (k==n)
+                prevf = nullptr, first = head;
+            while (i++<n-k && k!=n-1) {
+                prevf=prevf->next;
+                first=prevf->next;
+            }
+        } else {
+            i=0;
+            prevs = first;
+            while (i++<n-2*k) {
+                prevs=prevs->next;
+                second=prevs->next;
+            }
+        }
+        if(prevf) prevf->next = second;
+        if (first->next==second) {
+            first->next = second->next;
+            second->next=first;
+        } else {
+            swap(second->next,first->next);
+            prevs->next = first;
+        }
+        if (head==first) return second;
+        else return head;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
 int main() {
     auto s = new Solution();
-    int arr[] = {47,62,39,94,90,17,74,83,70,12,99,29,73};
+    int arr[] = {55,60,78,53,93,37,31,4,61,11,13,51,34,83,24,96,5,77,1,67};
 //    int arr[] = {1,2};
-    int n=13, k=1;
+    int n=20, k=11;
     ListNode l[n];
     l[n-1].val = arr[n-1]; l[n-1].next = nullptr;
     for (int i=n-2;i>=0;--i) l[i].val=arr[i], l[i].next = &l[i+1];
