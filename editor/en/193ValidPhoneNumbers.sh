ae4cefca2fd4f29bd11e05051b75468138a49283
Nikolopoulos Nikolaos <ndnikolop@di.uoa.gr>
2022-03-16 23:13:32 +0200

    193ValidPhoneNumbers egrep (faster)

--- editor/en/193ValidPhoneNumbers.sh
+++ editor/en/193ValidPhoneNumbers.sh
@@ -1,2 +1,2 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
-cat file.txt | while read line; do [[ $line =~ ^(\([0-9]{3}\)[[:space:]]|[0-9]{3}-)[0-9]{3}-[0-9]{4}$ ]] && echo $line; done
+egrep -i '^(\([0-9]{3}\)[[:space:]]|[0-9]{3}-)[0-9]{3}-[0-9]{4}$' file.txt
