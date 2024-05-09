705068298392ca79e8859227e693532d78d0a9eb
ntopower2 <itsnsquared@gmail.com>
2024-05-09 18:13:37 +0300

    daily problem maximumHappinessSum (sort)

--- 3075MaximizeHappinessofSelectedChildren.py
+++ 3075MaximizeHappinessofSelectedChildren.py
@@ -4,15 +4,16 @@ import heapq
 
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        happiness = [-h for h in happiness]
-        heapq.heapify(happiness)
-        res = i = 0
-        while happiness and happiness[0] + i < 0 and i < k:
-            res -= heapq.heappop(happiness) + i
-            i += 1
+        happiness.sort(reverse=True)
+        n = len(happiness)
+        res = 0
+        for i in range(k):
+            if (happiness[i] - i) <= 0:
+                return res
+            res += happiness[i] - i
         return res
 
 
-assert Solution().maximumHappinessSum([7, 50, 3], 3) == 57
+assert Solution().maximumHappinessSum([12, 1, 42], 3) == 53
 assert Solution().maximumHappinessSum([2, 3, 4, 5], 1) == 5
 assert Solution().maximumHappinessSum([1, 1, 1, 1], 2) == 1
