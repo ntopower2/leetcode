2f11fa3e56ca5c03ac76845b3da093a07d5962dd
ntopower2 <itsnsquared@gmail.com>
2024-06-18 14:52:13 +0300

    daily problem maxProfitAssignment
    simpler solution with sorting

--- editor/en/826MostProfitAssigningWork.py
+++ editor/en/826MostProfitAssigningWork.py
@@ -9,34 +9,20 @@ class Solution:
         if max(worker) < min(difficulty):
             return 0
         worker.sort()
-        res = 0
+        jobs = sorted(zip(difficulty, profit))
 
-        canDoMaxProfit = []
-        cannotDoMinDif = []
-        for d, p in zip(difficulty, profit):
-            if d <= worker[0]:
-                heappush(canDoMaxProfit, (-p, d))
-            else:
-                heappush(cannotDoMinDif, (d, p))
-
-        prof, _ = None, None
+        i = curProfit = res = 0
         for w in worker:
-            while cannotDoMinDif and cannotDoMinDif[0][0] <= w:
-                d, p = heappop(cannotDoMinDif)
-                heappush(canDoMaxProfit, (-p, d))
-            if not canDoMaxProfit:
-                if prof:
-                    res += prof
-                continue
-            if not prof or -canDoMaxProfit[0][0] > prof:
-                prof, _ = heappop(canDoMaxProfit)
-                prof = -prof
-            res += prof
+            while i < len(jobs) and jobs[i][0] <= w:
+                curProfit = max(curProfit, jobs[i][1])
+                i += 1
+            res += curProfit
+
         return res
 
 
-assert Solution().maxProfitAssignment([10, 55, 60], [46, 57, 96], [97, 31, 49]) == 188
 assert (
     Solution().maxProfitAssignment([2, 4, 6, 8, 10], [10, 20, 30, 40, 50], [4, 5, 6, 7])
     == 100
 )
+assert Solution().maxProfitAssignment([10, 55, 60], [46, 57, 96], [97, 31, 49]) == 188
