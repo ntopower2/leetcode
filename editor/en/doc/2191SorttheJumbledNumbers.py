e688a213388671745cf3ec1136e6f4dd119783ce
ntopower2 <itsnsquared@gmail.com>
2024-07-24 11:33:37 +0300

    daily problem sortJumbled (numeric conversion)

--- editor/en/doc/2191SorttheJumbledNumbers.py
+++ editor/en/doc/2191SorttheJumbledNumbers.py
@@ -7,16 +7,23 @@ class Solution:
         def mapNumFrom(num) -> int:
             if not mapping:
                 return num
-            s = str(num)
-            s2 = ""
-            for c in s:
-                s2 += str(mapping[int(c)])
-            return int(s2)
+            res = i = 0
+            d = num
+            if d == 0:
+                return mapping[0]
+            while d > 0:
+                d, m = divmod(d, 10)
+                res += mapping[m] * (10**i)
+                i += 1
+            return res
 
         nums.sort(key=mapNumFrom)
         return nums
 
 
+assert Solution().sortJumbled(
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+) == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 assert Solution().sortJumbled(
     mapping=[8, 9, 4, 0, 2, 1, 3, 5, 7, 6], nums=[991, 338, 38]
 ) == [338, 38, 991]
