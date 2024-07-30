96108d492d084b1f7a95944f2aa5252256833733
ntopower2 <itsnsquared@gmail.com>
2024-07-30 22:46:53 +0300

    daily problem minimumDeletions
    better solution with constant space

--- editor/en/1653minimumDeletions.py
+++ editor/en/1653minimumDeletions.py
@@ -1,16 +1,12 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        res = len(s)
-        before = [0] * (len(s) + 1)
-        count = 0
-        for i in range(len(s)):
-            before[i + 1] = before[i] + (s[i] == "b")
-            count += s[i] == "a"
-
-        for i in range(len(s)):
-            count -= s[i] == "a"
-            res = min(res, count + before[i])
-
+        res = bs = 0
+        for c in s:
+            if c == "b":
+                bs += 1
+            elif bs:
+                res += 1
+                bs -= 1
         return res
 
 
