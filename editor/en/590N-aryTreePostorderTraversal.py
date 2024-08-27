f3e492f0fc16d1c3d03ba2559dbda34ef8bf30ec
ntopower2 <itsnsquared@gmail.com>
2024-08-27 14:21:43 +0300

    daily problems postOrder

---
+++ editor/en/590N-aryTreePostorderTraversal.py
@@ -0,0 +1,20 @@
+# Definition for a Node.
+from typing import List
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root: "Node") -> List[int]:
+        if not root:
+            return []
+        res = []
+        for child in root.children:
+            res += self.postorder(child)
+        res += [root.val]
+
+        return res
