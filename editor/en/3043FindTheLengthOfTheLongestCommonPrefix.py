420709aeda9dfb4ddaedb2519f90aba353359ce3
ntopower2 <itsnsquared@gmail.com>
2024-09-24 14:55:22 +0300

    daily problem longestCommonPrefix (no trie)

--- editor/en/3043FindTheLengthOfTheLongestCommonPrefix.py
+++ editor/en/3043FindTheLengthOfTheLongestCommonPrefix.py
@@ -8,37 +8,23 @@
 from typing import List
 
 
-class TrieNode:
-    def __init__(self) -> None:
-        self.children = {}
-        self.isWord = False
-
-
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         if len(arr1) > len(arr2):
             return self.longestCommonPrefix(arr2, arr1)
 
-        root = TrieNode()
-        for word in arr1:
-            node = root
-            for c in str(word):
-                if c not in node.children:
-                    node.children[c] = TrieNode()
-                node = node.children[c]
-            node.isWord = True
+        prefixes = set()
+        for num in arr1:
+            while num:
+                prefixes.add(num)
+                num //= 10
 
-        res = tmp = 0
-        for word in arr2:
-            node = root
-            for c in str(word):
-                if c not in node.children:
-                    break
-                node = node.children[c]
-                tmp += 1
-
-            res = max(res, tmp)
-            tmp = 0
+        res = 0
+        for num in arr2:
+            while num and num not in prefixes:
+                num //= 10
+            if num in prefixes:
+                res = max(res, len(str(num)))
 
         return res
 
