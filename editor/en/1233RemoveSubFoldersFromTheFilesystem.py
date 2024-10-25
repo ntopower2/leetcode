bf0678b1963b9fee61db23e0b44a78e88425e69a
ntopower2 <itsnsquared@gmail.com>
2024-10-25 12:06:45 +0300

    daily problem removeSubfolders (strings)

--- editor/en/1233RemoveSubFoldersFromTheFilesystem.py
+++ editor/en/1233RemoveSubFoldersFromTheFilesystem.py
@@ -8,41 +8,22 @@
 from typing import List
 
 
-class TrieNode:
-    def __init__(self) -> None:
-        self.children = {}
-        self.end = False
-
-
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         res = []
-        tmp = None
+        prev = None
         folder.sort()
-        root = TrieNode()
 
         for path in folder:
-            parts = path.split("/")[1:]
-            cur = root
-
-            for i, part in enumerate(parts):
-                if part not in cur.children:
-                    cur.children[part] = TrieNode()
-                cur = cur.children[part]
-                if cur.end:
-                    tmp = i
-
-            if tmp is None or tmp == len(parts) - 1:
+            if not prev or not path.startswith(prev + "/"):
                 res.append(path)
-
-            cur.end = True
-            tmp = None
+                prev = path
 
         return res
 
 
 # @lc code=end
-assert Solution().removeSubfolders(["/ah/al/am", "/ah/al"]) == ["/ah/al"]
+assert Solution().removeSubfolders(["/a", "/a/b/c", "/a/b/d"]) == ["/a"]
 assert Solution().removeSubfolders(["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"]) == [
     "/a",
     "/c/d",
